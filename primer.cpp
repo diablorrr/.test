b911c9ffa474074169623d63486951b9eea48304
ysh <1299331829@qq.com>
2024-09-09 19:39:53 +0800

    add

--- primer.cpp
+++ primer.cpp
@@ -1,6 +1,7 @@
 #include "Person.h"
 #include "TextQuery.h"
 #include <fstream>
+#include <functional>
 #include <ostream>
 #include <string>
 #include <map>
@@ -16,34 +17,46 @@ using namespace std;
 
 //#include "String.h"
 
-class PrintString {
-	public:
-		PrintString(istream &i = cin): is(i) {}
-		string operator()() const {
-			string str;
-			getline(is, str);
-			return is ? str : string();
-		}
+
+
+class StrLenBetween {
 	private:
-		istream &is;
-};
-
-class check {
+		int minLen_;
+		int maxLen_;
 	public:
-		check(int v) : val(v) {}
-	bool operator()(int a) {return a == val;}
+		StrLenBetween(int minLne, int maxLen)
+			: minLen_(minLne), maxLen_(maxLen) {}
+		bool operator()(const string &str) {
+			return str.length() >= minLen_ && str.length() <= maxLen_;
+		}
+};
 
-	int val;
+class biggies {
+	public:
+		biggies(vector<string>::size_type s) : sz(s) {}
+		bool operator()(const string &a) {return a.size() >= sz;}
+	private:
+		vector<string>::size_type sz;
+};
+
+int add(int i, int j) {return i + j;}
+auto mod = [](int i, int j) {return i % j;};
+struct divide {
+	int operator()(int i, int j) {
+		return i / j;
+	}
 };
 
 
+struct LongDouble {
+	LongDouble(double = 0.0);
+	operator double();
+	operator float();
+};
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	vector<int> vec = {3, 2, 1, 4, 3, 7, 8, 6};
 
-	std::replace_if(vec.begin(), vec.end(), check(3), 5);
-	for (int i : vec) std::cout << i << " ";
-	std::cout << std::endl;
+
 }
